6a991d1b5619f6fafd0fb7922ef07d4fb998f072
Ty [Indev] <168133165+tyindev@users.noreply.github.com>
2024-06-25 00:53:15 -0400

    start

---
+++ dllmain.cpp
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include <iostream>
+
+DWORD WINAPI InitSwagShit(LPVOID)
+{
+    AllocConsole();
+
+    FILE* fptr;
+    freopen_s(&fptr, "CONOUT$", "w+", stdout);
+
+    SetConsoleTitleA("Mini FN");
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dllshits, LPVOID lpReserved)
+{
+    switch (dllshits)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, InitSwagShit, 0, 0, 0);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+    }
+    return TRUE;
+}
